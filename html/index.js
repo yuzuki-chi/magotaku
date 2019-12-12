1f6add7d500dbd607a7de7570dad60321448d1de
Yutaro-Takaya <c011719086@edu.teu.ac.jp>
2019-12-12 14:09:46 +0900

    Add Form program (phpfiles, jsfiles)

---
+++ html/index.js
@@ -0,0 +1,21 @@
+var subm = document.getElementById("form_subm");
+subm.addEventListener("click", function(){
+	// お名前
+	let name = document.getElementById("name").value;
+	// メールアドレス
+	let fromAddress = document.getElementById("email").value;
+	// 電話番号
+	let phone = document.getElementById("phone").value;
+	// 問い合わせ内容
+	let message = document.getElementById("message").value;
+	
+	let str = {'name':name, 'fromAddress':fromAddress, 'phone':phone, 'message':message};
+	let json = JSON.stringify(str);
+	let req = new XMLHttpRequest();
+	req.open('POST', './sendMail.php', true);
+	req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	req.send(json);
+	req.onload=function(){
+		alert(req.responseText);
+	}
+})
---
+++ html/sendMail.php
@@ -0,0 +1,30 @@
+<?php
+mb_language('japanese');
+mb_internal_encoding('UTF-8');
+if ($json = file_get_contents('php://input'))
+    $data = json_decode($json, true);
+
+//入力必須欄の確認
+if ($data['fromAddress']==null) exit('メールアドレスが未入力です');
+else if($data['name']==null) exit('お名前が未入力です');
+else if($data['phone']==null) exit('電話番号が未入力です');
+else if($data['message']==null) exit('問い合わせ内容が未入力です。');
+$fromAddress = $data['fromAddress'];
+$atAddress = "c011719086@edu.teu.ac.jp";
+$name = $data['name'];
+$phone = $data['phone'];
+$message = html_entity_decode($data['message']);
+
+$body = "お名前:".$name."<br/>".
+        "電話番号:".$phone."<br/>".
+        "メールアドレス:".$fromAddress."<br/>".
+        "問い合わせ内容:".htmlspecialchars($message, ENT_QUOTES, "UTF-8");
+
+try {
+	$success = mb_send_mail($atAddress, "問い合わせ", $body, 'From: ' . $fromAddress);
+	if($success){
+	    echo "success";
+	}
+} catch (Exception $e) {
+	echo "予期せぬエラーが発生しました";
+}

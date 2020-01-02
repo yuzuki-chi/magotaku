0b122bb3b91a432fb4b56ac8ff4b10bc470c364e
yuzuki-chi <c011719086@edu.teu.ac.jp>
2020-01-02 17:42:22 +0900

    change js path from young, old create

---
+++ html/js/create_young.js
@@ -0,0 +1,28 @@
+//YOUNG SUBMIT BUTTON
+var young_create_subm = document.getElementById("young_create_subm");
+young_create_subm.addEventListener("click", function(){
+	// お名前
+    let name = document.getElementById("name").value;
+    //　年齢
+    let age = document.getElementById("age").value;
+    // 性別
+    let flex = document.getElementById("flex").value;
+    // 住所
+    let address = document.getElementById("address").value;
+	// メールアドレス
+	// let fromAddress = document.getElementById("email").value;
+	// 電話番号
+	// let phone = document.getElementById("phone").value;
+	// プロフィール（？）
+	let message = document.getElementById("message").value;
+	
+	let str = {'name':name, 'age':age, 'flex':flex, 'address':address, 'message':message};
+	let json = JSON.stringify(str);
+	let req = new XMLHttpRequest();
+	req.open('POST', 'https://citron-tree.jp/private/magotaku_tmp/young_create.php', true);
+	req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	req.send(json);
+	req.onload=function(){
+		alert(req.responseText);
+	}
+})
